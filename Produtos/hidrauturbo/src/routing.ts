0d319373a4819fb0d761390e98cc8257a2bec285
MayconAguiar <maycon.aguiar@gmail.com>
2018-03-15 20:04:47 -0300

    alteracoes no carrossel.

--- Produtos/hidrauturbo/src/routing.ts
+++ Produtos/hidrauturbo/src/routing.ts
@@ -12,12 +12,13 @@ import { EmpresaComponent } from './app/empresa/empresa.component';
 import { PecasComponent } from './app/pecas/pecas.component';
 import { ContatoComponent } from './app/contato/contato.component';
 import { LocalizacaoComponent } from './app/localizacao/localizacao.component';
+import { CarrosselComponent } from './app/carrossel/carrossel.component';
 
 const APP_ROUTES: Routes = [
     { path: "", component: PrincipalComponent},
     { path: "gallery", component: GalleryComponent},
     { path: "image/:id", component: ImageComponent},
-    { path: "carrossel", component: CarrosselFotosComponent},
+    { path: "carrossel", component: CarrosselComponent},
     { path: "servicos", component: ServicosComponent},
     { path: "empresa", component: EmpresaComponent},
     { path: "pecas", component: PecasComponent},
