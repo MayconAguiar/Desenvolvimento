02c942849f1919f58f2a2bcff94fe23d2a57009a
maycon <maycon.aguiar@gmail.com>
2018-10-10 10:38:55 -0300

    reorganização e incuslão de cards

--- Produtos/CalculaImposto/src/app/app.module.ts
+++ Produtos/CalculaImposto/src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MomentModule} from 'angular2-moment/moment.module';
-import {NgbModule, NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
-
-import { NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,11 +11,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppComponent
   ],
   imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserModule, MomentModule, NgbModule
+    BrowserModule, MomentModule
   ],
-  providers: [NgbDropdownConfig, NgbTooltipConfig],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
