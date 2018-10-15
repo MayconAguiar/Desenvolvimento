fcc72aa80215639b04e4c4a1d30e70493f1d08a5
maycon <maycon.aguiar@gmail.com>
2018-10-15 17:51:47 -0300

    atualização

--- Produtos/CalculaImposto/src/app/arquivosPDF.ts
+++ Produtos/CalculaImposto/src/app/arquivosPDF.ts
@@ -17,28 +17,8 @@ export class ArquivosPDF {
 
     private leiaArquivo(files, index, observer: Subscriber<any>) {
         const PDFJS: PDFJSStatic = require('pdfjs-dist');
-        debugger;
-        
-        // PDFJS.GlobalWorkerOptions.workerSrc = '../../node_modules/pdfjs-dist/build/pdf.worker.entry.js';
-        PDFJS.GlobalWorkerOptions.workerSrc = '../assets/pdf.worker.min.js';
 
-        // if (index < files.length) {
-        //     const file: File = files.item(index);
-        //     const reader: FileReader = new FileReader();
-        //     reader.readAsText(file);
-        //     const that = this;
-        //     reader.onload = (e) => {
-        //         debugger;
-        //         const linha = reader.result;
-        //         index += 1;
-        //         this.lista.push(that.csvJSON(linha));
-        //         this.leiaArquivo(files, index, observer);
-        //     };
-        // } else {
-        //     observer.next(this.lista);
-        //     observer.complete();
-        // }
-        // PDFJS.workerSrc = '/static/js/pdf.worker.js';
+        PDFJS.GlobalWorkerOptions.workerSrc = '../assets/pdf.worker.min.js';
 
         if (index < files.length) {
 
@@ -48,19 +28,18 @@ export class ArquivosPDF {
              const that = this;
 
              reader.onload = (event: any) => {
-                 debugger;
                  const typedarray = event.target.result;
-                 //const typedarray = new Uint8Array(event.target.result);
 
-                 PDFJS.getDocument(typedarray).then((pdf) => {
-                     debugger;
-                    console.log(pdf);
+                this.ObtenhaDocumento(PDFJS, typedarray).then(texts => {
+                    for (let y = 0; y < texts.length; y++) {
+                        const element = texts[y];
+                        this.lista.push(element);
+                    }
                     index += 1;
                     this.leiaArquivo(files, index, observer);
-                 });
-                 
-                 
-                 //// this.lista.push(that.csvJSON(linha));
+                });
+
+
 
              };
 
@@ -69,6 +48,76 @@ export class ArquivosPDF {
              observer.next(this.lista);
              observer.complete();
         }
+    }
+
+    private ObtenhaDocumento(PDFJS, typedarray) {
+        const that = this;
+        return PDFJS.getDocument(typedarray).then( pdf => {
+
+
+            const maxPages = pdf.pdfInfo.numPages;
+            const countPromises = []; // collecting all page promises
+            const listaDeLinhas = [];
+            let lastY = -1;
+
+            for (let j = 1; j <= maxPages; j++) {
+            
+                
+               const page = pdf.getPage(j);
+
+               countPromises.push(page.then(function(pageInterno) { // add page promise
+                
+
+                    const textContent = pageInterno.getTextContent();
+
+                    return textContent.then(function(text) { // return content promise
+                        
+                        let listaItens = [];
+
+                        text.items.forEach(function (i) {
+
+                         // Tracking Y-coord and if changed create new p-tag
+                         lastY = lastY === -1 ? i.transform[5] : lastY;
+                         const novaLinha = lastY !== i.transform[5];
+
+                         if (novaLinha) {
+                           // listaDeLinhas.push(i.textContent);
+                           lastY = i.transform[5];
+                           that.adicioneLinha(listaItens, listaDeLinhas);
+                           listaItens = [];
+                         }
+
+                         listaItens.push(i.str);
+                       });
+
+                       that.adicioneLinha(listaItens, listaDeLinhas);
+
+                       return listaDeLinhas;
+                   });
+
+               }));
+            }
+            // Wait for all pages and join text
+            return Promise.all(countPromises).then(function (listas) {
+                const novalista = [];
+
+                for (let i = 0; i < listas.length; i++) {
+                    const lista = listas[i];
+                    for (let j = 0; j < lista.length; j++) {
+                        const element = lista[j];
+                        novalista.push(element);
+                    }
+                }
+                console.log(listas);
+                console.log(novalista);
+            });
+        });
+
+    }
+
+    private adicioneLinha(listaItens: any[], listaDeLinhas: any[]) {
+        const linha = listaItens.join(' ');
+        listaDeLinhas.push(linha);
     }
 
     private csvJSON(csv) {
